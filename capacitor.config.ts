0bb3a73cbbfe5a8476a967c8f30ba27cbb72cd61
Paulo Martins <ph.mar89s@gmail.com>
2026-05-26 16:58:05 -0300

    fix: renomear app para SmartPro

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.paulo_martins.controlefinanceiros',
-  appName: 'Dividi Aí',
+  appName: 'SmartPro',
   webDir: 'dist',
   android: {
     buildOptions: {
